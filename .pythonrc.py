d092cb2b9f4fab3e87c417fb7bbaca26f1225604
Mike Barton <mike@weirdlooking.com>
2011-07-23 06:28:24 +0000

    pythonrc changes

--- .pythonrc.py
+++ .pythonrc.py
@@ -9,29 +9,26 @@ def __setup():
     import atexit
     import sys
     import pprint
+    import readline
+    import rlcompleter
 
+    HISTORYFILE = os.path.join(os.environ['HOME'], '.python-history')
+    if os.uname()[0] == 'Darwin':
+        readline.parse_and_bind("bind ^I rl_complete")
+    else:
+        readline.parse_and_bind("tab: complete")
     try:
-        import readline
-        import rlcompleter
-        HISTORYFILE = os.path.join(os.environ['HOME'], '.python-history')
-        if os.uname()[0] == 'Darwin':
-            readline.parse_and_bind("bind ^I rl_complete")
-        else:
-            readline.parse_and_bind("tab: complete")
-        try:
-            readline.read_history_file(HISTORYFILE)
-        except IOError:
-            pass  # It doesn't exist yet.
-        atexit.register(lambda: readline.write_history_file(HISTORYFILE))
-    except ImportError:
-        print "Unable to enable tab completion.  You'll probably need readline."
+        readline.read_history_file(HISTORYFILE)
+    except IOError:
+        pass  # It doesn't exist yet.
+    atexit.register(lambda: readline.write_history_file(HISTORYFILE))
 
     sys.ps1 = '\n\x1b[0;32m \x1b[1;32m>>\x1b[0;32m \x1b[0m: '
     sys.ps2 = '  ... '
 
     _pprinter = pprint.PrettyPrinter(indent=4).pprint
     def pprinter(value):
-        __builtins__._ = value
+        __builtins__._ = value # populate _ with result of previous statement
         sys.stdout.write('\x1b[0;31m \x1b[1;31m<<\x1b[0;31m \x1b[0m: ')
         _pprinter(value)
     sys.displayhook = pprinter
